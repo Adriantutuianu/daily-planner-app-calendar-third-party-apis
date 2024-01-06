916111bb303448cc79ecdca7a4998806d0a2e807
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-06 22:56:12 +0000

    Created functionality for time and date update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
-// display current day using dayjs
-const currentDay = dayjs().format("dddd, MMMM DD");
-const curDay = $("#currentDay").text(currentDay);
+//display currentDay and currentTime dayjs
+const update = function () {
+  // display current day using dayjs
+  const currentDay = dayjs().format("dddd, MMMM DD");
+  $("#currentDay").text(currentDay);
 
-//display current time using dayjs
-const currentTime = dayjs().format("h:mm:ss a");
-const curTime = $("#currentTime").text(currentTime);
+  //display current time using dayjs
+  const currentTime = dayjs().format("h:mm:ss a");
+  $("#currentTime").text(currentTime);
+};
+update();
+
+//setinterval for update
+setInterval(update, 1000);
